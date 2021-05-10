5836570f9eb5c6a324ad492bd039490b9e2df71f
Yuta Katayama <yutapilafu@gmail.com>
2021-05-10 21:30:07 +0900

    コンポーネント(single file component)はケバブケースorパスカルケースを使う（おすすめはパスカルケース）

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div>
+    <!-- 余談:DOMテンプレート(.htmlファイル)では必ずケバブケースを使うようにする必要があるが、それはHTML→JSの順番でブラウザがファイルを読み込みむが、その際にパスカルケースの大文字小文字は区別されず、意図しない動きになるから 
+         Vue.jsのsingle file componentでは、Vue側のrender関数がよしなにやってくれるので気にしなくてOK -->
+    <!-- <like-header></like-header> ケバブケース -->
     <LikeHeader></LikeHeader>
     <LikeNumber></LikeNumber>
   </div>
@@ -11,7 +14,7 @@ import LikeHeader from "./components/LikeHeader";
 export default {
   components: {
     // LikeHeader: LikeHeader,
-    LikeHeader, // ES6の書き方（key・valueが同じなら省略可）
+    LikeHeader, // パスカルケース
   },
 };
 </script>
