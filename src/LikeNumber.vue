bc25235f080c7a03c592fe2b43adc5348cf8c4b5
Yuta Katayama <yutapilafu@gmail.com>
2021-05-10 20:57:26 +0900

    templateルートは1つの要素にしなければならない

--- src/LikeNumber.vue
+++ src/LikeNumber.vue
@@ -1,5 +1,13 @@
 <template>
-  <p>いいね({{ number }})</p>
+  <!-- template内は1つの要素のみで構成されないとエラーになるので以下はNG
+       ErrorMsg : The template root requires exactly one element
+       ので、divタグで囲む必要がある -->
+  <!-- <p>いいね({{ number }})</p>
+  <button @click="increment">+1</button> -->
+  <div>
+    <p>いいね({{ number }})</p>
+    <button @click="increment">+1</button>
+  </div>
 </template>
 
 <script>
@@ -8,6 +16,11 @@ export default {
     return {
       number: 5,
     };
+  },
+  methods: {
+    increment() {
+      this.number += 1;
+    },
   },
 };
 </script>
