11a9a2f4c91c96e2988b6884affa6b3476d5a4f2
Yuta Katayama <yutapilafu@gmail.com>
2021-05-18 23:16:28 +0900

    子→親へのコンポーネント間のデータ等の受け渡しは<slot>でも不可

--- src/components/LikeHeader.vue
+++ src/components/LikeHeader.vue
@@ -1,13 +1,18 @@
 <template>
   <div>
-    <!-- vue.jsのhtmlを子コンポーネントに受け渡すには<slot>を用いる -->
     <slot></slot>
+    <p>{{ headerText }}</p>
   </div>
 </template>
 
 <script>
 export default {
-  // propsはあくまでデータ（オブジェクト）のみの受け渡ししかできない
   props: ["headerText"],
 };
 </script>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
