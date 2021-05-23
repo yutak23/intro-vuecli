248cc55fe9d9fe7b177669ebc4d15bdb093242f9
Yuta Katayama <yutapilafu@gmail.com>
2021-05-23 19:01:35 +0900

    複数のslotがある（名前付きスロットも混在する）場合にはデフォルトスロットのスロットプロパティの省略記法は使えない

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
     <!-- templateが1つ（デフォルト）しかない場合、子コンポーネントにv-slotを記述する事でスロットプロパティを使える -->
-    <!-- <LikeHeader v-slot:default="slotProps"> ←1つしかtemplateしかない時、それはdefaultなのでそれも省略可能-->
-    <LikeHeader v-slot="slotProps">
-      <p>{{ slotProps }}</p>
-      <h2>みなさん</h2>
-      <h4>いいねをたくさんもらいましょう</h4>
-      <p>よろしくお願いします</p>
+    <!-- 以下のように他のtemplate(slot)がある場合、省略記法は使えないので注意。以下のエラーが出る。
+         Default slot must use '<template>' on a custom element when there are other named slots.
+           
+         <LikeHeader v-slot="slotProps"> ←他のtemplate（slot）があるのでこれは使えない
+         他のtemplate（slot）が存在する場合は以下のようにデフォルトスロットを明示的に定義する必要がある -->
+    <LikeHeader>
+      <template v-slot:default="slotProps">
+        <p>{{ slotProps }}</p>
+        <h2>みなさん</h2>
+        <h4>いいねをたくさんもらいましょう</h4>
+        <p>よろしくお願いします</p>
+      </template>
+      <template v-slot:title="slotProps2">
+        <p>{{ slotProps2 }}</p>
+      </template>
     </LikeHeader>
     <LikeNumber :total-number="number" @my-click="number = $event"></LikeNumber>
     <LikeNumber :total-number="number" @my-click="increaseNumber"></LikeNumber>
