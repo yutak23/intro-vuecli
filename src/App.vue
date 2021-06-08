2e4d61a1963fd5037cf340d374eb173945980881
Yuta Katayama <yutapilafu@gmail.com>
2021-06-08 23:13:59 +0900

    親・子のコンポーネント間でv-modelを使う

--- src/App.vue
+++ src/App.vue
@@ -11,33 +11,11 @@
     </keep-alive>
     <div>
       <h2>イベントのフォーム</h2>
-      <p>タイトル</p>
-      <div class="form-floating mb-3">
-        <input
-          type="text"
-          class="form-control"
-          id="title"
-          placeholder="タイトル"
-          v-model="eventData.title"
-        />
-        <label for="title">タイトル</label>
-      </div>
-
-      <!-- 上記のv-modelを使った双方向データバインディングは、以下のように":valueと@input"で書き換えできる -->
-      <!-- ※<input>タグでtype="text"の場合の話でその他のチェックボックス・ラジオボタンなどでは違うので注意 -->
-      <!-- （ちなみに、v-model.lazyは@changeで同じ事が実現できる） -->
-      <div class="form-floating mb-3">
-        <input
-          type="text"
-          class="form-control"
-          id="title2"
-          placeholder="タイトル"
-          :value="eventData.title"
-          @input="eventData.title = $event.target.value"
-        />
-        <label for="title2">タイトル</label>
-      </div>
-      <p>{{ eventData.title }}</p>
+      <EventTitle v-model="eventData.title"></EventTitle>
+      <!-- <EventTitle
+        :value="eventData.title"
+        @input="eventData.title = $event"
+      ></EventTitle> -->
     </div>
   </div>
 </template>
@@ -46,6 +24,7 @@
 import LikeHeader from "./components/LikeHeader.vue";
 import About from "./components/About";
 import Home from "./components/Home";
+import EventTitle from "./components/EventTitle.vue";
 
 export default {
   data() {
@@ -61,6 +40,7 @@ export default {
     LikeHeader,
     About,
     Home,
+    EventTitle,
   },
   methods: {
     increaseNumber(value) {
---
+++ src/components/EventTitle.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>タイトル</p>
+    <div class="form-floating mb-3">
+      <input
+        type="text"
+        class="form-control"
+        id="title"
+        placeholder="タイトル"
+        :value="value"
+        @input="$emit('input', $event.target.value)"
+      />
+      <label for="title">タイトル</label>
+    </div>
+    <p>{{ value }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["value"],
+};
+</script>
