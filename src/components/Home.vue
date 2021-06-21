2553c7b8d6efbf7aa02fb8e94eafd378df8b58a8
Yuta Katayama <yutapilafu@gmail.com>
2021-06-21 21:45:16 +0900

    カスタムディレクティブに引数を渡す

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <p v-border="{ width: '5px', color: 'red' }">Home</p>
+  <p v-border:dotted="{ width: '5px', color: 'red' }">Home</p>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -8,15 +8,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
 
-// bindとupdateは基本同じ内容になる
-// ex）値を受け取って赤色とか黄色に変えるとかは、bind・updateの両方でやる事になりほぼ同じ内容になる
-// ので、bind・updateは1つの関数として書ける
 Vue.directive('border', function (el, binding) {
-  // el : カスタムディレクティブを定義している要素そのもの
-  // 今回で言えば "<p></p>" がelになるので以下のようにそのstyleを指定するとかできる
-  el.style.border = 'solid black 2px';
   el.style.borderWidth = binding.value.width;
   el.style.borderColor = binding.value.color;
+
+  // カスタムディレクティブの引数（:hogeのように:(コロン)の後に続くもの）を受け取るにはbinding.argを使う
+  // ※ただし、引数は1つしか取れない
+  el.style.borderStyle = binding.arg;
 });
 
 new Vue({
