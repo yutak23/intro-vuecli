ccce39eaced46e89134f5114ea7e6f1a16e05755
Yuta Katayama <yutapilafu@gmail.com>
2021-06-21 21:56:57 +0900

    コンポーネントのdirectivesオプションでカスタムディレクティブをローカル登録する

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,5 +13,22 @@ export default {
   activated() {
     console.log("Home activated");
   },
+  // カスタムディレクティブをローカル登録する方法は以下
+  // ※ただし、これはこのコンポーネントでしか使えないのでそこは要注意
+  directives: {
+    // border: {}, <- このようにオブジェクトで定義してbind, inserted, update, …をそれぞれ定義する事もできる
+    border(el, binding) {
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.5)";
+      }
+    },
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -8,19 +8,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
 
-Vue.directive('border', function (el, binding) {
-  el.style.borderWidth = binding.value.width;
-  el.style.borderColor = binding.value.color;
-  el.style.borderStyle = binding.arg;
+// Vue.directive('border', (el, binding) => {
+//   el.style.borderWidth = binding.value.width;
+//   el.style.borderColor = binding.value.color;
+//   el.style.borderStyle = binding.arg;
 
-  // カスタムディレクティブで修飾子の有無は、binding.modifiers.hogeで確かめられる（修飾子ありならtrue）
-  if (binding.modifiers.round) {
-    el.style.borderRadius = '0.5rem';
-  }
-  if (binding.modifiers.shadow) {
-    el.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.5)';
-  }
-});
+//   if (binding.modifiers.round) {
+//     el.style.borderRadius = '0.5rem';
+//   }
+//   if (binding.modifiers.shadow) {
+//     el.style.boxShadow = '0 2px 5px rgba(0, 0, 0, 0.5)';
+//   }
+// });
 
 new Vue({
   render: h => h(App),
