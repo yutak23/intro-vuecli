0db1c316faa4a9a884951709a5d4cc714c0301a0
Yuta Katayama <yutapilafu@gmail.com>
2021-06-22 23:38:53 +0900

    グローバルミックスイン

--- src/components/CountNumber.vue
+++ src/components/CountNumber.vue
@@ -11,20 +11,12 @@
 import { tokyoNumber } from "@/tokyoNumber";
 
 export default {
-  /**
-   * 以下のようにミックスインで定義されているオプションと同じオプションがコンポーネント側に定義されている場合、
-   * コンポーネントのオプション内容で上書きされる
-   */
   mixins: [tokyoNumber],
   data() {
     return {
       title: "Welcome to Los",
     };
   },
-  /**
-   * ライフサイクルフックについては例外で、ミックスインとコンポーネントの両方が実行される
-   * ミックスインのライフサイクルフックメソッドを実行した後にコンポーネントのライフサイクルフックメソッドが実行される
-   */
   created() {
     console.log("created in component");
   },
--- src/main.js
+++ src/main.js
@@ -8,6 +8,21 @@ import "bootstrap/dist/css/bootstrap.min.css";
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
 
+/**
+ * 以下のようにミックスインをグローバル登録すると、
+ * 全てのコンポーネントでそのミックスインがマージ(適用)されるので使う際には要注意
+ * コンポーネントにマージ(適用)される場合、以下の順番になる
+ * 1.グローバルミックスインのオプション -> 2.ローカルミックスインのオプション -> 3.コンポーネントのオプション
+ *
+ * ※基本的にグローバルミックスインは使わない
+ *   使う場面は、プラグインなどで全てのコンポーネントに適用させたい何かを定義した時などに限定される 
+ */
+Vue.mixin({
+  created() {
+    console.log("global mixin");
+  }
+});
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
