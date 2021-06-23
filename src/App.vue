ba27c2147df98ed55b65a5092871e9ba782e0b2f
Yuta Katayama <yutapilafu@gmail.com>
2021-06-23 21:39:14 +0900

    PJを一度きれいきれい

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,15 @@
 <template>
   <div class="container-sm">
-    <LikeHeader>
-      <h3>はじめまして</h3>
-    </LikeHeader>
-    <LikeNumber :total-number="number" @my-click="increaseNumber"></LikeNumber>
-    <div class="mt-5">
-      <button
-        type="button"
-        class="btn btn-primary me-2"
-        @click="currenComponent = 'Home'"
-      >
-        Home
-      </button>
-      <button
-        type="button"
-        class="btn btn-primary me-2"
-        @click="currenComponent = 'About'"
-      >
-        About
-      </button>
-      <keep-alive>
-        <component :is="currenComponent" class="mt-3"></component>
-      </keep-alive>
-    </div>
-    <div class="mt-5">
-      <h2>イベントのフォーム</h2>
-      <EventTitle v-model="eventData.title"></EventTitle>
-    </div>
+    <hello-world></hello-world>
   </div>
 </template>
 
 <script>
-import LikeHeader from "./components/LikeHeader.vue";
-import About from "./components/About";
-import Home from "./components/Home";
-import EventTitle from "./components/EventTitle.vue";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  data() {
-    return {
-      number: 10,
-      currenComponent: "Home",
-      eventData: {
-        title: "",
-      },
-    };
-  },
   components: {
-    LikeHeader,
-    About,
-    Home,
-    EventTitle,
-  },
-  methods: {
-    increaseNumber(value) {
-      console.log(`increaseNumber : ${value}`);
-      this.number = value;
-    },
+    HelloWorld,
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import LikeNumber from './components/LikeNumber.vue'
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 Vue.config.productionTip = false
-Vue.component('LikeNumber', LikeNumber);
-Vue.mixin({
-  created() {
-    console.log("global mixin");
-  }
-});
 
 new Vue({
   render: h => h(App),
