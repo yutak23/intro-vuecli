762e10639dfc25a074a31869407a30e8fb4afea6
Yuta Katayama <yutapilafu@gmail.com>
2021-06-24 21:48:53 +0900

    Vue.jsでのCSSアニメーション

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,11 @@
     <button type="button" class="btn btn-primary" @click="show = !show">
       切り替え
     </button>
-    <!-- トランジションのname属性がない場合、デフォルトではname="v"になるのでcssもv-…のようになる -->
     <transition name="fade">
       <p v-if="show">hello</p>
+    </transition>
+    <transition name="slide">
+      <p v-if="show">bye</p>
     </transition>
   </div>
 </template>
@@ -22,31 +24,49 @@ export default {
 
 <style scoped>
 .fade-enter {
-  /* 現れる時の最初の状態をここに定義する */
   opacity: 0;
 }
 .fade-enter-active {
-  /* 現れる時（現れている最中）のトランジション・アニメーションの状態をここに定義する 
-     何秒間かけてどのようなアニメーションを付けたいのか？  */
   transition: opacity 5s;
 }
 .fade-enter-to {
-  /* 現れる時の最後の状態をここに定義する */
-  opacity: 1; /* ← 本来これはデフォルトなので宣言する必要はないが今回はあえて */
+  opacity: 1;
 }
 .fade-leave {
-  /* 消える時の最初の状態をここに定義する */
-  opacity: 1; /* ← 本来これはデフォルトなので宣言する必要はないが今回はあえて */
+  opacity: 1;
 }
 .fade-leave-active {
-  /* 消える時（消えている最中）のトランジション・アニメーションの状態をここに定義する 
-     何秒間かけてどのようなアニメーションを付けたいのか？  */
   transition: opacity 5s;
 }
 .fade-leave-to {
-  /* 消える時の最後の状態をここに定義する */
   opacity: 0;
 }
+
+/* enter, enter-to, leave, leave-toは、keyframesに指定しているので定義する必要がない（Vue.jsが勝手にやってくれる） */
+/* .slide-enter {
+} */
+.slide-enter-active {
+  animation: slide-in 0.5s;
+}
+/* .slide-enter-to {
+}
+.slide-leave {
+} */
+.slide-leave-active {
+  animation: slide-in 0.5s reverse;
+}
+/* .slide-leave-to {
+} */
+
+@keyframes slide-in {
+  from {
+    transform: translateX(100px);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+
 .main {
   width: 70%;
   margin: auto;
