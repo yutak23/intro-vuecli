995393818cfca934700708e6a8bc5b1de659960a
Yuta Katayama <yutapilafu@gmail.com>
2021-06-28 21:09:44 +0900

    JavaScriptでトランジション・アニメーションを実装する時の基本型

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,20 @@
     <button type="button" class="btn btn-primary me-1" @click="show = !show">
       切り替え
     </button>
+    <br /><br />
+    <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @enter-cancelled="enterCancelled"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @leave-cancelled="leaveCancelled"
+    >
+      <div class="circle" v-if="show"></div>
+    </transition>
+    <br />
     <button
       type="button"
       class="btn btn-primary me-1"
@@ -53,6 +67,10 @@ import ComponentA from "./components/ComponentA.vue";
 import ComponentB from "./components/ComponentB.vue";
 
 export default {
+  components: {
+    ComponentA,
+    ComponentB,
+  },
   data() {
     return {
       show: true,
@@ -60,9 +78,40 @@ export default {
       myComponent: "ComponentA",
     };
   },
-  components: {
-    ComponentA,
-    ComponentB,
+  methods: {
+    /*
+     * JavaScriptsでアニメーション（JavaScriptフック関数）を作る時の型
+     */
+    beforeEnter(el) {
+      // 現れる前
+    },
+    enter(el, done) {
+      // 現れる時（イメージはtransitionのCSSの"-enter-active"の部分）
+      // 引数のdone：done()の用に関数として宣言し、アニメーションが終わった事をVue.jsに伝えるための関数
+      // done()関数の役割：非同期処理を実行できるようにするためのもの
+      // ※CSSのアニメーションが定義されている場合にはdone()は省略可能（CSSのアニメーションが未定義ならdone()は必須）
+      // ※CSS・JavaScriptのアニメーションの両方を定義して同時に使える。done()がない場合は両方のアニメーションが実行されるが、done()がある場合はCSSのアニメーションが実行されていてもdone()が呼ばれた時点でアニメーションは強制終了になる
+    },
+    afterEnter(el) {
+      // 現れた後
+    },
+    enterCancelled(el) {
+      // 現れるトランジション・アニメーションがキャンセルされた時
+      // ex) アニメーション実行中にDOMが消えたりしてアニメーションが実行できなくなった時
+    },
+    beforeLeave(el) {
+      // 消える前
+    },
+    leave(el, done) {
+      // 消える時（イメージはtransitionのCSSの"-leave-active"の部分）
+    },
+    afterLeave(el) {
+      // 消えた後
+    },
+    leaveCancelled(el) {
+      // 消えるトランジション・アニメーションがキャンセルされたとき
+      // ※v-showの時だけ実行される
+    },
   },
 };
 </script>
@@ -114,5 +163,13 @@ export default {
   margin: auto;
   padding-top: 5rem;
   text-align: center;
+}
+
+.circle {
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  border-radius: 100px;
+  background-color: deeppink;
 }
 </style>
