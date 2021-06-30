6204f94c0c4af6f9205e644cfdca8228b214bb93
Yuta Katayama <yutapilafu@gmail.com>
2021-06-30 21:37:47 +0900

    router-linkの属性2つ（active-classとexact)

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,22 @@
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav">
             <li class="nav-item">
-              <router-link class="nav-link" to="/">Home</router-link>
+              <router-link
+                class="nav-link"
+                to="/"
+                exact
+                active-class="link--active"
+                >Home
+              </router-link>
             </li>
             <li class="nav-item">
-              <router-link class="nav-link" to="/users">Users</router-link>
+              <router-link
+                class="nav-link"
+                to="/users"
+                exact
+                active-class="link--active"
+                >Users
+              </router-link>
             </li>
           </ul>
         </div>
@@ -33,3 +45,9 @@
     </main>
   </div>
 </template>
+
+<style scoped>
+.link--active {
+  font-size: 20px;
+}
+</style>
