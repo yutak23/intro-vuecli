acc7728a68450e6804d6933606fe1ade239bd896
Yuta Katayama <yutapilafu@gmail.com>
2021-07-04 20:12:28 +0900

    vue-routerの動的URLでパラメータが変わった時はライフサイクルフックが呼ばれないので注意

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,7 +1,24 @@
 <template>
   <div>
     <h3>Users</h3>
+    <router-link to="/users/1">ユーザ１</router-link>
+    <router-link to="/users/2">ユーザ２</router-link>
     <hr />
     <h1>User No. {{ $route.params.id }}</h1>
   </div>
 </template>
+
+<script>
+export default {
+  created() {
+    console.log("created");
+  },
+  watch: {
+    $route(to, from) {
+      console.log("$routeのwatch");
+      console.log(to);
+      console.log(from);
+    },
+  },
+};
+</script>
