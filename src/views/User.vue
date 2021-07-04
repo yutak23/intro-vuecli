6035c8cd81fef39a989b6d3087f1459a61035f2c
Yuta Katayama <yutapilafu@gmail.com>
2021-07-04 21:46:21 +0900

    v-bindでto属性を動的に定義する

--- src/views/User.vue
+++ src/views/User.vue
@@ -5,6 +5,9 @@
     <router-link to="/users/2">ユーザ２</router-link>
     <hr />
     <h1>User No. {{ id }}</h1>
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile'"
+      >次のユーザ</router-link
+    >
     <router-view></router-view>
   </div>
 </template>
