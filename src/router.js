8c99cc18104352601fc013d089ccc88fe2c166b2
Yuta Katayama <yutapilafu@gmail.com>
2021-07-04 20:36:01 +0900

    vue-routerのprops　を使わないようにしルートとコンポーネ ント間の密結合をなくす

--- src/router.js
+++ src/router.js
@@ -10,6 +10,6 @@ export default new Router({
     mode: 'history',
     routes: [
         { path: '/', component: Home },
-        { path: '/users/:id', component: Users }
+        { path: '/users/:id', component: Users, props: true }
     ]
 });
--- src/views/User.vue
+++ src/views/User.vue
@@ -4,21 +4,13 @@
     <router-link to="/users/1">ユーザ１</router-link>
     <router-link to="/users/2">ユーザ２</router-link>
     <hr />
-    <h1>User No. {{ $route.params.id }}</h1>
+    <h1>User No. {{ id }}</h1>
   </div>
 </template>
 
 <script>
 export default {
-  created() {
-    console.log("created");
-  },
-  watch: {
-    $route(to, from) {
-      console.log("$routeのwatch");
-      console.log(to);
-      console.log(from);
-    },
-  },
+  /** propsで定義する変数は、vue-routerのパスに書いた":(コロン)～"の～の部分を書く */
+  props: ["id"],
 };
 </script>
