ce2b90d4c1f218d413f0fae247a45fdf086b4049
Yuta Katayama <yutapilafu@gmail.com>
2021-07-05 22:12:03 +0900

    router-viewにトランジションを適用する

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
   <div>
-    <!-- 名前付きビューを使う事で、各<router-view></router-view>に異なるコンポーネントを設定する事ができるようになる -->
     <router-view name="header"></router-view>
     <main class="container-sm">
-      <router-view></router-view>
+      <!-- router-viewはある意味動的コンポーネントみたいなものなのでトランジションも適応可能 -->
+      <transition name="fade" mode="out-in">
+        <router-view></router-view>
+      </transition>
     </main>
   </div>
 </template>
+
+<style scoped>
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+</style>
