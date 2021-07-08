7b8fab765e1d02c3d2707d140d2f6680b4c69ad2
Yuta Katayama <yutapilafu@gmail.com>
2021-07-08 14:56:58 +0900

    gettersにプロパティを定義し、storeの値を取得できるようにする

--- src/store.js
+++ src/store.js
@@ -6,5 +6,15 @@ Vue.use(Vuex);
 export default new Vuex.Store({
     state: {
         count: 2
+    },
+    getters: {
+        // gettersを定義する事でどのコンポーネントでもVuexのStoreの値を取得するものが作れる
+        // gettersはコンポーネントのcomputedとほぼ同じような使い方をするイメージでOK
+        doubleCount: state => state.count * 2
+
+        // ちなみに、ES6のアロー関数を使わないと以下のように書く事になる
+        // doubleCount(state) {
+        //     return state.count * 2
+        // }
     }
 });
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,17 @@
     <button type="button" class="btn btn-primary" @click="toUsers">
       Usersのページに行く
     </button>
-    <p>{{ count }}</p>
+    <p>{{ doubleCount }}</p>
   </div>
 </template>
 
 <script>
 export default {
   computed: {
-    count() {
-      return this.$store.state.count;
+    doubleCount() {
+      // 以下はどちらも同じ結果だが、Vuexのgettersを使う事でコンポーネント側で勝手に処理を書き換えられるというのを防げるメリットがある
+      // return this.$store.state.count * 2;
+      return this.$store.getters.doubleCount;
     },
   },
   methods: {
