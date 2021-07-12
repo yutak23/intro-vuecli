091502b1ccbbeec5cc90a3f1e7506792e46941fb
Yuta Katayama <yutapilafu@gmail.com>
2021-07-12 21:59:47 +0900

    axiosのinstallとPJ準備

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-    <HelloWorld></HelloWorld>
+  <div class="container-sm mt-5">
+    <h3>掲示板に投稿する</h3>
+    <form>
+      <div class="mb-3">
+        <label for="name" class="form-label">ニックネーム</label>
+        <input type="text" class="form-control" id="name" v-model="name" />
+      </div>
+      <div class="mb-3">
+        <label for="comment" class="form-label">コメント</label>
+        <textarea class="form-control" id="comment" v-model="comment" />
+      </div>
+    </form>
+    <h2>掲示板</h2>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      name: "",
+      comment: "",
+    };
   },
 };
 </script>
