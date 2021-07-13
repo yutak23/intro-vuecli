7ef53e9efc82bcbf7f5a1a5a99d6dffce0fa6fc3
Yuta Katayama <yutapilafu@gmail.com>
2021-07-13 21:54:31 +0900

    interceptorsでメソッド実行の前・後に処理を割り込みさせる

--- src/main.js
+++ src/main.js
@@ -14,6 +14,36 @@ axios.defaults.baseURL = "https://firestore.googleapis.com/v1/projects/vuejs-htt
 // axios.defaults.headers.common["Authorization"] = "hogehoge";
 // axios.defaults.headers.get["Accept"] = "application/json";
 
+// axios.getなどが呼ばれたときに、まずこのinterceptors.requestの内容が実行され、その後axios.getの内容が実行される
+// 使い処：tokenを付けるなどの処理を実装する
+//        エラー発生時にモーダルを画面に表示させる
+const interceptersRequest = axios.interceptors.request.use(
+  config => {
+    // このようにHTTP requestのconfigを修正したりできる
+    // config.headers["hoge"] = "hoge";
+    console.log("interceptors request", config);
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+// axios.getなどが呼ばれて何かしらが返ってきた時（.then()のres）、まずこのinterceptors.responseの内容が実行され、その後axios.getの.then(res => ...)のresにreturnの値が返る
+const interceptersResponse = axios.interceptors.response.use(
+  response => {
+    console.log("interceptors response", response);
+    return response;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+// axios.interceptors.requestのidをeject()の第1引数に渡す事で、axiosのuseを取り消す処理を書く事ができる
+axios.interceptors.request.eject(interceptersRequest);
+axios.interceptors.response.eject(interceptersResponse);
+
 new Vue({
   render: h => h(App),
 }).$mount("#app")
