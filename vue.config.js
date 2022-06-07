a0147563c9ec8df6a872ed7a04446cf259786c7b
Yuta Katayama <yutapilafu@gmail.com>
2022-06-07 09:14:54 +0900

    feat: version.jsonを見れるようにする

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+const path = require('path');
 const { LicenseWebpackPlugin } = require('license-webpack-plugin');
+const VersionFile = require('webpack-version-file');
+const { DateTime } = require('luxon');
+
+const dt = DateTime.now();
 
 module.exports = {
 	configureWebpack: {
@@ -7,7 +12,23 @@ module.exports = {
 				unacceptableLicenseTest: (licenseType) =>
 					['GPL', 'AGPL', 'LGPL', 'NGPL'].includes(licenseType),
 				outputFilename: 'meta/license.txt'
+			}),
+			new VersionFile({
+				output: './dist/version.json',
+				templateString: `{${['version', 'buildDate', 'timestamp', 'environment']
+					.map((key) => `"${key}": "<%= ${key} %>"`)
+					.join(',')}}`,
+				data: {
+					buildDate: dt.toISO(),
+					timestamp: dt.toSeconds(),
+					environment: process.env.NODE_ENV || 'development'
+				}
 			})
 		]
+	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, 'dist')
+		}
 	}
 };
